43cb8d6f3be2b2742137f782d4913f136c46f439
limelytecoder <thelimelyteinbox@gmail.com>
2025-06-10 12:45:25 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const io = socketIo(server);
 app.use(express.static('public'));
 
 io.on('connection', socket => {
-  socket.on('offer', (data) => socket.broadcast.emit('offer', data));
-  socket.on('answer', (data) => socket.broadcast.emit('answer', data));
-  socket.on('ice-candidate', (data) => socket.broadcast.emit('ice-candidate', data));
+  socket.on('offer', data => socket.broadcast.emit('offer', data));
+  socket.on('answer', data => socket.broadcast.emit('answer', data));
+  socket.on('ice-candidate', data => socket.broadcast.emit('ice-candidate', data));
 });
 
 server.listen(3000, () => console.log('Server running on http://localhost:3000'));
